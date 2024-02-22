b652bde48cde64eb56be927dc5799d0d014dfaf9
Pau Botargues Julio <pbotargu@car1s5.42barcelona.com>
2024-02-22 18:15:43 +0100

    error_and_free.c norminette ok

--- src/error_and_free.c
+++ src/error_and_free.c
@@ -6,18 +6,18 @@
 /*   By: pbotargu <pbotargu@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 11:14:05 by pbotargu          #+#    #+#             */
-/*   Updated: 2024/02/22 17:54:51 by pbotargu         ###   ########.fr       */
+/*   Updated: 2024/02/22 18:14:56 by pbotargu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/so_long.h"
 
-void    ft_error_free(char *message, t_data *info)
+void	ft_error_free(char *message, t_data *info)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    if (info->map)
+	i = 0;
+	if (info->map)
 	{
 		while (info->map[i])
 		{
@@ -26,41 +26,40 @@ void    ft_error_free(char *message, t_data *info)
 		}
 		free(info->map);
 	}
-    write(2, "Error\n", 6);
-    i = 0;
-    while (message[i] != '\0')
-    {
-        write(2, &message[i], 1);
-        i++;
-    }
-    write(2, "\n", 1);
-    exit (1);
+	write(2, "Error\n", 6);
+	i = 0;
+	while (message[i] != '\0')
+	{
+		write(2, &message[i], 1);
+		i++;
+	}
+	write(2, "\n", 1);
+	exit (1);
 }
 
-void    ft_error(char *message)
+void	ft_error(char *message)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    write(2, "Error\n", 6);
-    while (message[i] != '\0')
-    {
-        write(2, &message[i], 1);
-        i++;
-    }
-    write(2, "\n", 1);
-    exit (1);
+	i = 0;
+	write(2, "Error\n", 6);
+	while (message[i] != '\0')
+	{
+		write(2, &message[i], 1);
+		i++;
+	}
+	write(2, "\n", 1);
+	exit (1);
 }
 
-int ft_close(t_data *info, int i)
+int	ft_close(t_data *info, int i)
 {
-    if (i == 1)
-    {
-        ft_printf("Number of movements: %d\n", info->movements);
-        ft_printf("\n🧙🏼YOU'VE CATCH THE SNITCH🧙🏼\n");
-        ft_printf("\nThanks for playing so_long by @pbotargu\n\n");
-    }
-    mlx_destroy_window(info->mlx, info->mlxw);
-    exit(0);
+	if (i == 1)
+	{
+		ft_printf("Number of movements: %d\n", info->movements);
+		ft_printf("\n🧙🏼YOU'VE CATCH THE SNITCH🧙🏼\n");
+		ft_printf("\nThanks for playing so_long by @pbotargu\n\n");
+	}
+	mlx_destroy_window(info->mlx, info->mlxw);
+	exit(0);
 }
-
